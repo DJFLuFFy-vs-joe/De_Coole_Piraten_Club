4e9f6d91db3746f8b37e07362ad1008a8edee810
DJFLuFFy_vs_joe <dj.fluffy@gmail.com>
2015-02-24 20:13:12 +0100

    Added Skyblock_intro.mp3 and i created a special class for playing that song.

---
+++ app/src/main/java/nl/vsjoe/android/decoolepiratenclub/musicplayer/SkyBlockThemeSongPlayer.java
@@ -0,0 +1,29 @@
+package nl.vsjoe.android.decoolepiratenclub.musicplayer;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.view.View;
+
+import nl.vsjoe.android.decoolepiratenclub.R;
+
+/**
+ * Created by FLuFFy on 24-2-2015.
+ */
+public class SkyBlockThemeSongPlayer {
+
+    private MediaPlayer mp;
+
+    public SkyBlockThemeSongPlayer(Context context) {
+
+        mp = new MediaPlayer().create(context, R.raw.skyblock_intro);
+    }
+
+    public void playIntro() {
+
+        if (mp.isPlaying()) {
+            mp.stop();
+        }
+
+        mp.start();
+    }
+}
